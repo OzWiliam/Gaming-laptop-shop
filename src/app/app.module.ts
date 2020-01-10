d2af01f1bdfa5a1930d269463d6d990b86768980
Oz Wiliam <32298573+OzWiliam@users.noreply.github.com>
2020-01-10 16:28:19 +0700

    Revert "add frame project"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BannerComponent } from './banner/banner.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductInputComponent } from './product-input/product-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BannerComponent,
-    FooterComponent,
-    ProductListComponent,
-    ProductInputComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
