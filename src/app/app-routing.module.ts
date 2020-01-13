9d6bfa05522bf5fd65d6a66db2087fa146f08756
Oz Wiliam <it98learning@gmail.com>
2020-01-13 17:07:14 +0700

    [UI] Integrate API & UI for get & insert product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from "./product-list/product-list.component";
+import { AppComponent } from "./app.component";
+import { ProductInputComponent } from "./product-input/product-input.component";
 
+const routes: Routes = [
+  { path: '', redirectTo: '/productlist', pathMatch: 'full'},
+  { path: 'productlist', component: ProductListComponent },
+  { path: 'productinput', component: ProductInputComponent },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductInputComponent } from './product-input/product-input.component';
+import { ProductComponent } from './product/product.component';
+import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,13 +16,18 @@ import { ProductInputComponent } from './product-input/product-input.component';
     BannerComponent,
     FooterComponent,
     ProductListComponent,
-    ProductInputComponent
+    ProductInputComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HttpClient, 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
