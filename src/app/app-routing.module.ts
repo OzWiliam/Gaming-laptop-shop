4a6b709cb4136427afca3de39e983a56277a6c17
Oz Wiliam <it98learning@gmail.com>
2020-01-20 15:25:32 +0700

    [UI] IAPI & UI for get, insert and delete product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { ProductListComponent } from "./product-list/product-list.component";
 import { AppComponent } from "./app.component";
 import { ProductInputComponent } from "./product-input/product-input.component";
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CategoryInputComponent } from "./category-input/category-input.component";
+import { CategoryDetailComponent } from "./category-detail/category-detail.component";
+import { CategoryUpdateComponent } from "./category-update/category-update.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/productlist', pathMatch: 'full'},
+  { path: '', redirectTo: '/productlist', pathMatch: 'full' },
   { path: 'productlist', component: ProductListComponent },
   { path: 'productinput', component: ProductInputComponent },
   { path: 'detail/:_id', component: ProductDetailComponent },
+  { path: 'categorydetail', component: CategoryDetailComponent },
+  { path: 'detail-category/:id', component: CategoryUpdateComponent }
 ];
 
 @NgModule({
---
+++ src/app/category-update/category-update.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from "../category.service";
+import { Category } from "../category";
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-category-update',
+  templateUrl: './category-update.component.html',
+  styleUrls: ['./category-update.component.css']
+})
+export class CategoryUpdateComponent implements OnInit {
+  category: Category;
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    const id = this.route.snapshot.paramMap.get('_id');
+    // console.log('Da goi ', id);
+    this.categoryService.getCategoryById(id).subscribe((category: Category) => {
+      // console.log(category);
+      this.category = Object.assign(category[0]);
+    }, (error) => {
+      console.log('ERROR ', error);
+    })
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    console.log('called', this.category)
+    this.categoryService.updateCategory(this.category).subscribe(
+    )
+  }
+}
