2cdd3d754174851237e0a13c57cafc13176b7658
Oz Wiliam <it98learning@gmail.com>
2020-02-26 09:12:53 +0700

    release product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
 import { CategoryInputComponent } from "./category-input/category-input.component";
 import { CategoryDetailComponent } from "./category-detail/category-detail.component";
 import { CategoryUpdateComponent } from "./category-update/category-update.component";
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/productlist', pathMatch: 'full' },
@@ -14,7 +15,8 @@ const routes: Routes = [
   { path: 'productinput', component: ProductInputComponent },
   { path: 'detail/:_id', component: ProductDetailComponent },
   { path: 'categoryinput', component: CategoryInputComponent },
-  { path: 'detail-category/:_id', component: CategoryUpdateComponent }
+  { path: 'detail-category/:_id', component: CategoryUpdateComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
